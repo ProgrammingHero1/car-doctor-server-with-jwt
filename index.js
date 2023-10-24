615b0076fbf619cbb97f7f9eb43df84a44a4b4eb
Programming Hero <programming.hero1@gmail.com>
2023-10-24 10:12:21 -0500

    simple token without secret

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -31,6 +32,15 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        // auth related api
+        app.post('/jwt', async(req, res) =>{
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
+            res.send(token)
+        })
+
+        // services related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
