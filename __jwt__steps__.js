a85d8a62ebfe5f7d5c4f0f0a9b77ef9cc18e0378
Programming Hero <programming.hero1@gmail.com>
2023-10-25 08:53:09 -0500

    set token to the cookies

---
+++ __jwt__steps__.js
@@ -0,0 +1,24 @@
+/****
+ * install jsonwebtoken
+ * jwt.sign (payload, secret, {expiresIn:})
+ * token client 
+ * 
+ * 
+*/
+
+
+/***
+ * how to store token in the client side
+ * 1. memory --> ok type
+ * 2. local storage -->ok type (XSS)
+ * 3. cookies: http only
+*/
+
+/**
+ * 1. set cookies with http only. for development secure: false, 
+ * 
+ * 2. cors
+ * 
+ * 3. client side axios setting
+*/
+
--- index.js
+++ index.js
@@ -36,8 +36,15 @@ async function run() {
         app.post('/jwt', async(req, res) =>{
             const user = req.body;
             console.log(user);
-            const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
-            res.send(token)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+
+            res
+            .cookie('token', token, {
+                httpOnly: true,
+                secure: false, // http://localhost:5174/login,
+                sameSite: 'none'
+            })
+            .send({success: true})
         })
 
         // services related api
